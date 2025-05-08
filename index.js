ee3afd18edcc48537001d91f6114e7213ad7721a
abdurrahman2004 <ar978874@gmail.com>
2025-05-08 21:20:36 +0530

    Test version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import express from 'express';
 const app = express();
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello from Jenkins CI/CD!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
