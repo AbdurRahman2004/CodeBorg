7f483e8c9f878e784aa16a168c994c786ad52252
abdurrahman2004 <ar978874@gmail.com>
2025-05-27 20:18:42 +0530

    first commit

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import express from 'express';
 const app = express();
 app.get('/', (req, res) => res.send('Hello from Jenkins CI/CD!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
+
+
+//85cd2df2226de9ac7bf1013b0008ff2fe7982cee
